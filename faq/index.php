11860795326cf8418bd1d2ac48b2a70a84eb3780
cassiopaia <cassiopia@riseup.net>
2014-04-19 17:42:48 +0200

    FAQ - Accordion Integration
    ...

--- faq/index.php
+++ faq/index.php
@@ -5,97 +5,171 @@
       <br/>
       <br/>
       <center><h1><span style="color:#9ce936;">bit</span><span style="color:#009df6;">shares</span> FAQ</h1></center>
-      <ul>
-        <a name="crypto-equity"></a>
-        <li>
-            <h3>What is a DAC?</h3>
-            <p>
-              DAC stands for Decentralized Autonomous Company or previously Distributed Autonomous Corporation.  DACs are
-              not legal entities and instead serve as a metaphor for describing how transparent, irreversable ledger systems such as Bitcoin
-              operate.  DACs can have expenses (paying miners) and they can earn revenue (charging transaction fees). 
-            </p>
-        </li>
-
-            <a style="margin-top:-100px" name="bips"></a>
-        <li>
-            <h3>What is a crypto-equity?</h3>
-            <p>
-            A crypto-equity is a more accurate description of what a crypto-currency is.  Bitcoin is first and foremost a crypto-equity, but when it
-            is used as a medium of exchange, it becomes a crypto-currency.  In the same ways shares in Apple (AAPL) are first and foremost
-            an equity, but if they became <a href="http://www.investopedia.com/terms/b/bearer_share.asp">bearer shares</a> and started circulating 
-            in barter transactions they could become a currency as well.
-            </p>
-        </li>
-        <li>
-            <h3>What is a BIP?</h3>
-            <p>
-            A BIP is one billionth of the current share supply and is the fundamental unit of account in a DAC.  Given 8,000,000 initial shares, a
-            BIP would be .008 shares. The total number of shares in circulation is constantly decreasing as <a href="#dividend">dividends</a> are
-            paid from transaction fees.
-            </p>
-            <p>
-            While the number of shares in your wallet doesn't change, the number of BIPs in your wallet is constantly growing and in this 
-            way users have a better unit of account.   The user interface of all DACs should completely hide the underlying shares and
-            instead display all balances in termps of BIPs and all transfers should also be specified in terms of BIPs.
-            </p>
-        </li>
-        <li>
-            <a name="dividends"><h3>What are Dividends?</h3></a>
-            <p>
+      <div class="panel-group" id="accordion">
+      
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h4 class="panel-title">
+              <a data-toggle="collapse" data-parent="#accordion" href="#collapse1">
+                What is a DAC?
+              </a>
+            </h4>
+          </div>
+          <div id="collapse1" class="panel-collapse collapse in">
+            <div class="panel-body">
+             DAC stands for Decentralized Autonomous Company or previously Distributed Autonomous Corporation.  DACs are
+             not legal entities and instead serve as a metaphor for describing how transparent, irreversable ledger systems such as Bitcoin
+             operate.  DACs can have expenses (paying miners) and they can earn revenue (charging transaction fees). 
+            </div>
+          </div>
+        </div>
+        
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h4 class="panel-title">
+              <a data-toggle="collapse" data-parent="#accordion" href="#collapse2">
+                What is a crypto-equity?
+              </a>
+            </h4>
+          </div>
+          <div id="collapse2" class="panel-collapse collapse">
+            <div class="panel-body">
+             A crypto-equity is a more accurate description of what a crypto-currency is.  Bitcoin is first and foremost a crypto-equity, but when it
+             is used as a medium of exchange, it becomes a crypto-currency.  In the same ways shares in Apple (AAPL) are first and foremost
+             an equity, but if they became <a href="http://www.investopedia.com/terms/b/bearer_share.asp">bearer shares</a> and started circulating 
+             in barter transactions they could become a currency as well.
+            </div>
+          </div>
+        </div>
+        
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h4 class="panel-title">
+              <a data-toggle="collapse" data-parent="#accordion" href="#collapse3">
+                What is a BIP?
+              </a>
+            </h4>
+          </div>
+          <div id="collapse3" class="panel-collapse collapse">
+            <div class="panel-body">
+            
+             A BIP is one billionth of the current share supply and is the fundamental unit of account in a DAC.  Given 8,000,000 initial shares, a
+             BIP would be .008 shares. The total number of shares in circulation is constantly decreasing as <a href="#dividend">dividends</a> are
+             paid from transaction fees.
+            <br><br>
+             While the number of shares in your wallet doesn't change, the number of BIPs in your wallet is constantly growing and in this 
+             way users have a better unit of account.   The user interface of all DACs should completely hide the underlying shares and
+             instead display all balances in termps of BIPs and all transfers should also be specified in terms of BIPs.
+          
+            </div>
+          </div>
+        </div>
+      
+      
+      <div class="panel panel-default">
+          <div class="panel-heading">
+            <h4 class="panel-title">
+              <a data-toggle="collapse" data-parent="#accordion" href="#collapse4">
+                What are Dividends?
+              </a>
+            </h4>
+          </div>
+          <div id="collapse4" class="panel-collapse collapse">
+            <div class="panel-body">
             In the context of a DAC a 'dividend' is a metaphor for the increase in the number of <a href="#bips">bips</a> contained in your
             wallet as the result of the share supply decreasing.   The share supply decreases anytime transaction fees are paid by those
             making transactions.
-            </p>
-        </li>
-        <li>
-            <h3>What is a proto-DAC?</h3>
-            <p>
-            A proto-DAC is a company that doesn't implement the full feature set of the ultimate company, but which allows the trading
-            of shares in one or more future companies while they are under development.  <a href="/bitshares/ags-pts.php">bitshares pts</a>
-            is a proto-DAC for the entire industry.
-            </p>
-        </li>
-        <li>
-            <a name="mining"><h3>How do I mine bitshares?</h3></a>
-            <p>
-            Bitshares are not a single crypto-equity and therefore asking "how do I mine bitshares?" is like asking "how do I earn money?". Do you
-            want to earn US dollars or Euros?  Depending upon which DACs you would like to get a stake in there are four ways to mine:
-            <ol>
-              <li> Mine <a href="/bitshares/ags-pts.php">bitshares pts</a> for a stake in future DACs </li>
-              <li> Mine with <a href="/bitshares/ags-pts.php">bitshares ags</a> by giving to bitshares.org</li>
-              <li> A <a href="https://bitsharestalk.org/index.php?topic=3587.0;topicseen">Virtual Mining Pool</a> is under development.</li>
-              <li> Mine the most profitable altcoin and trade for what you really want.</li>
-            </ol>
-            </p>
-        </li>
-
-        <li>
-            <a name="bitshares-formatting"><h3>BitShares vs bitshares vs Bitshares vs bitShares which way is the right way?</h3></a>
-            <p>
-            This is a good question and it depends upon who you ask.  We currently recommend all lowercase all the time with the exception
-            of the start of sentences.  
-            </p>
-        </li>
-        <li>
-            <a name="proof-of-stake"><h3>What is Proof of Stake?</h3></a>
-            <p>
-            Proof of stake is the concept that certain actions can only be performed by a shareholder proportional their stake (<a href="#bips">bips</a> owned).
-            Read more about <a href="/security/proof-of-stake">proof of stake systems</a>.
-            </p>
-        </li>
-        <li>
-            <a name="bitasset"><h3>What is a BitAsset?</h3></a>
-            <p>
-            BitAssets are an experimental digital asset class that are fungible and market-pegged to the value of anything from dollars, to gold, to gallons of gasoline. BitAssets can be transferred between users in the same way as bitcoins.
-            </p>
-        </li>
-        <li>
-            <a name="short"><h3>What does shorting mean?</h3></a>
-            <p>
-	    Shorting an asset means borrowing it, selling it, then buying it back and returning it. This allows you to profit when you predict the value of an asset will fall. The BitShares X network issues BitAssets to people who take out short positions, using XTS (shares in bitshares X) as collateral.
-            </p>
-        </li>
-      </ul>
+            </div>
+          </div>
+        </div>
+     
+      <div class="panel panel-default">
+          <div class="panel-heading">
+            <h4 class="panel-title">
+              <a data-toggle="collapse" data-parent="#accordion" href="#collapse5">
+                How do I mine bitshares?
+              </a>
+            </h4>
+          </div>
+          <div id="collapse5" class="panel-collapse collapse">
+            <div class="panel-body">
+             Bitshares are not a single crypto-equity and therefore asking "how do I mine bitshares?" is like asking "how do I earn money?". Do you
+             want to earn US dollars or Euros?  Depending upon which DACs you would like to get a stake in there are four ways to mine:
+             <ol>
+               <li> Mine <a href="/bitshares/ags-pts.php">bitshares pts</a> for a stake in future DACs </li>
+               <li> Mine with <a href="/bitshares/ags-pts.php">bitshares ags</a> by giving to bitshares.org</li>
+               <li> A <a href="https://bitsharestalk.org/index.php?topic=3587.0;topicseen">Virtual Mining Pool</a> is under development.</li>
+               <li> Mine the most profitable altcoin and trade for what you really want.</li>
+             </ol>
+             
+            </div>
+          </div>
+        </div>
+        
+        <div class="panel panel-default">
+            <div class="panel-heading">
+              <h4 class="panel-title">
+                <a data-toggle="collapse" data-parent="#accordion" href="#collapse6">
+                  BitShares vs bitshares vs Bitshares vs bitShares which way is the right way?
+                </a>
+              </h4>
+            </div>
+            <div id="collapse6" class="panel-collapse collapse">
+              <div class="panel-body">
+             	This is a good question and it depends upon who you ask.  We currently recommend all lowercase all the time with the exception
+             	of the start of sentences.  
+              </div>
+            </div>
+          </div>
+          
+          <div class="panel panel-default">
+              <div class="panel-heading">
+                <h4 class="panel-title">
+                  <a data-toggle="collapse" data-parent="#accordion" href="#collapse7">
+                    What is Proof of Stake?
+                  </a>
+                </h4>
+              </div>
+              <div id="collapse7" class="panel-collapse collapse">
+                <div class="panel-body">
+                  Proof of stake is the concept that certain actions can only be performed by a shareholder proportional their stake (<a href="#bips">bips</a> owned).
+                  Read more about <a href="/security/proof-of-stake">proof of stake systems</a>.
+                </div>
+              </div>
+            </div>
+            
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                  <h4 class="panel-title">
+                    <a data-toggle="collapse" data-parent="#accordion" href="#collapse8">
+						What is a BitAsset?
+                    </a>
+                  </h4>
+                </div>
+                <div id="collapse8" class="panel-collapse collapse">
+                  <div class="panel-body">
+                   		BitAssets are an experimental digital asset class that are fungible and market-pegged to the value of anything from dollars, to gold, to gallons of gasoline. BitAssets can be transferred between users in the same way as bitcoins.
+                  </div>
+                </div>
+              </div>
+              
+              <div class="panel panel-default">
+                  <div class="panel-heading">
+                    <h4 class="panel-title">
+                      <a data-toggle="collapse" data-parent="#accordion" href="#collapse9">
+                        What does shorting mean?
+                      </a>
+                    </h4>
+                  </div>
+                  <div id="collapse9" class="panel-collapse collapse">
+                    <div class="panel-body">
+                      Shorting an asset means borrowing it, selling it, then buying it back and returning it. This allows you to profit when you predict the value of an asset will fall. The BitShares X network issues BitAssets to people who take out short positions, using XTS (shares in bitshares X) as collateral.
+                    </div>
+                  </div>
+                </div>
+               
+                  </div>
+ 
     </div>
 </div>
 <?php include '../footer.php'?>

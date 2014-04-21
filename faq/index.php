295e10e8c442579adc7592037e34124d43303d34
Nikolai Mushegian <nikolai@bitshares.org>
2014-04-21 05:13:47 +0000

    update dns and faq

--- faq/index.php
+++ faq/index.php
@@ -5,8 +5,12 @@
       <br/>
       <br/>
       <center><h1><span style="color:#9ce936;">bit</span><span style="color:#009df6;">shares</span> FAQ</h1></center>
+
+      <center><h3>DACs and BitShares</h3></center>
+	<hr>
       <div class="panel-group" id="accordion">
-      
+
+           
         <div class="panel panel-default">
           <div class="panel-heading">
             <h4 class="panel-title">
@@ -45,16 +49,50 @@
         <div class="panel panel-default">
           <div class="panel-heading">
             <h4 class="panel-title">
+              <a data-toggle="collapse" data-parent="#accordion" href="#collapse50">
+                What is a blockchain?
+              </a>
+            </h4>
+          </div>
+          <div id="collapse50" class="panel-collapse collapse">
+            <div class="panel-body">
+	   A blockchain is a public ledger that records who owns how many tokens of a crypto-equity; it is the basic building block of any crypto-equity. Blockchains are created and updated in a decentralized way; the crypto-equity's protocol is designed in a way that thousands of independent computers can all come to a consensus on what data should be in the blockchain. 
+            </div>
+          </div>
+        </div>
+
+ 
+        
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h4 class="panel-title">
               <a data-toggle="collapse" data-parent="#accordion" href="#collapse3">
-                What is a BIP?
+                Is bitshares a cryptocurrency?
               </a>
             </h4>
           </div>
           <div id="collapse3" class="panel-collapse collapse">
             <div class="panel-body">
+           A crypto-currency is a term coined to differenciate Bitcoin from other digital currencies such as Linden Dollars or egold. The defintion of crypto-currency as a digital medium of exchange is broad enough to cover BitShares; however, within the BitShares ecosystem we prefer to refer to systems such as Bitcoin and BitShares as a crypto-equity. 
+            </div>
+          </div>
+        </div>
+      
+ 
+        
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h4 class="panel-title">
+              <a data-toggle="collapse" data-parent="#accordion" href="#collapse51">
+                What is a BIP?
+              </a>
+            </h4>
+          </div>
+          <div id="collapse51" class="panel-collapse collapse">
+            <div class="panel-body">
             
              A BIP is one billionth of the current share supply and is the fundamental unit of account in a DAC.  Given 8,000,000 initial shares, a
-             BIP would be .008 shares. The total number of shares in circulation is constantly decreasing as <a href="#dividend">dividends</a> are
+             BIP would be .008 shares. The total number of shares in circulation is constantly decreasing as dividends are
              paid from transaction fees.
             <br><br>
              While the number of shares in your wallet doesn't change, the number of BIPs in your wallet is constantly growing and in this 
@@ -69,12 +107,12 @@
       <div class="panel panel-default">
           <div class="panel-heading">
             <h4 class="panel-title"><a name="dividends"></a>
-              <a data-toggle="collapse" data-parent="#accordion" href="#collapse4">
+              <a data-toggle="collapse" data-parent="#accordion" href="#dividend">
                 What are Dividends?
               </a>
             </h4>
           </div>
-          <div id="collapse4" class="panel-collapse collapse">
+          <div id="dividend" class="panel-collapse collapse">
             <div class="panel-body">
             In the context of a DAC a 'dividend' is a metaphor for the increase in the number of <a href="#bips">bips</a> contained in your
             wallet as the result of the share supply decreasing.   The share supply decreases anytime transaction fees are paid by those
@@ -132,12 +170,17 @@
               </div>
               <div id="collapse7" class="panel-collapse collapse">
                 <div class="panel-body">
-                  Proof of stake is the concept that certain actions can only be performed by a shareholder proportional their stake (<a href="#bips">bips</a> owned).
+                  Proof of stake is the concept that certain actions can only be performed by a shareholder in proportion to to their stake (<a href="#bips">bips</a> owned).
                   Read more about <a href="/security/proof-of-stake">proof of stake systems</a>.
                 </div>
               </div>
             </div>
-            
+    
+	  </div>
+ 
+      <center><h3>BitShares X</h3></center>
+      <hr>
+      <div class="panel-group" id="accordion">
             <div class="panel panel-default">
                 <div class="panel-heading">
                   <h4 class="panel-title">
@@ -167,9 +210,216 @@
                     </div>
                   </div>
                 </div>
-               
-                  </div>
+           
+	</div> <!-- accordion -->
+
  
+        <center><h3 id="faq-dns">BitShares DNS and .p2p</h3></center>
+        <hr>
+        <div class="panel-group" id="accordion">
+
+		  
+		  <div class="panel panel-default">
+		    <div class="panel-heading">
+		      <h4 class="panel-title">
+		        <a data-toggle="collapse" data-parent="#accordion" href="#faq-p2p">
+		         What is .p2p?
+		        </a>
+		      </h4>
+		    </div>
+		    
+		    <div id="faq-p2p" class="panel-collapse collapse">
+		      <div class="panel-body">
+		      .p2p is a new TLD which uses blockchain technology in place of traditional DNS. It is similar to Namecoin and the .bit project.
+		      </div>
+		      
+		    </div>
+		  </div>
+		  
+		  <div class="panel panel-default">
+		  
+		    <div class="panel-heading">
+		      <h4 class="panel-title">
+		        <a data-toggle="collapse" data-parent="#accordion" href="#faq-advantages">
+		          What are the advantages of .p2p over traditional DNS?
+		        </a>
+		      </h4>
+		    </div>
+		    
+		    <div id="faq-advantages" class="panel-collapse collapse">
+		      <div class="panel-body">
+		        Traditional DNS leaves its users vulnerable to man-in-the-middle attacks and domain name seizure. In addition, domain name registrars have become bloated and inefficient, extracting huge margins from locked-in services.
+		      </div>
+		      
+		    </div>
+		  </div>
+		  
+		  <div class="panel panel-default">
+		  
+		    <div class="panel-heading">
+		      <h4 class="panel-title">
+		        <a data-toggle="collapse" data-parent="#accordion" href="#faq-mitm">
+		          How does .p2p prevent MITM attacks?
+		        </a>
+		      </h4>
+		    </div>
+		    
+		    <div id="faq-mitm" class="panel-collapse collapse">
+		      <div class="panel-body">
+		        The owner of a domain name can put their SSL key directly into a secure public ledger, rather than relying on trusted certificate authorities to sign off on keys. There are no entities that can be coerced to put a compromised certificate in the record except fo the owner of the corresponding private keys.
+		      </div>
+		  
+		    
+		  </div>
+		</div>
+		
+		<div class="panel panel-default">
+		
+		  <div class="panel-heading">
+		    <h4 class="panel-title">
+		      <a data-toggle="collapse" data-parent="#accordion" href="#faq-seizures">
+		      How does .p2p prevent domain seizures?
+		      </a>
+		    </h4>
+		  </div>
+		  
+		  <div id="faq-seizures" class="panel-collapse collapse">
+		    <div class="panel-body">
+		    Just like bitcoin, .p2p operates in a completely decentralized fashion outside of any individual jurisdiction. There are no entities who can be coerced to change the value of the record except for the owner of the corresponding private keys.
+		    </div>
+		    
+		  </div>
+		</div>
+		
+		<div class="panel panel-default">
+		
+		  <div class="panel-heading">
+		    <h4 class="panel-title">
+		      <a data-toggle="collapse" data-parent="#accordion" href="#faq-squatting">
+		        How does .p2p disincentivise squatting?
+		      </a>
+		    </h4>
+		  </div>
+		  
+		  <div id="faq-squatting" class="panel-collapse collapse">
+		    <div class="panel-body">
+		    The combination of price discovery via an auction and opportunity cost of purchasing a domain (lost dividends from equivalent value of domainshares) makes it more profitable to hold DNS than to hold domains speculatively. This is the only source of revenue for the network, aside from standard per-byte transaction fees. The cost of renewing and updating domains is thus resource-driven.
+		    </div>
+		    
+		  </div>
+		</div>
+		
+		<div class="panel panel-default">
+		
+		  <div class="panel-heading">
+		    <h4 class="panel-title">
+		      <a data-toggle="collapse" data-parent="#accordion" href="#faq-differnt">
+		        How is Namecoin and .bit different from .p2p?
+		      </a>
+		    </h4>
+		  </div>
+		  
+		  <div id="faq-differnt" class="panel-collapse collapse">
+		    <div class="panel-body">
+		    <p>The critical difference is in the design of the relationship of the crypto-equity ("namecoins" or NMC for Namecoin, "nameshares", ".P2Pshares" or .P2P for .p2p) to the functionality of the network. We use the Distributed Autonomous Company metaphor to help describe how these two network reward their "shareholder" when it is able to provide value. This provides an incentive for engineers all around the globe to independently cooperate on maintaining the network with a narrowly defined goal and incentive mechanic.</p>
+		    <ul>
+		        <li>Namecoin issues new coins to miners as a reward for performing merged mining with the Bitcoin network. The namecoin supply is being inflated at nearly 30% per year for the next 40,000 blocks, then over 10% for the next four years. .p2p uses DPOS for significantly cheaper security, while domainshares only ever shrink in supply, when fees are destroyed as implicit dividends.</li>
+		        <li>Namecoin attempts to service multiple namespaces at once, including non-TLD key-value records. Domainshares is highly specialized for servicing the .p2p TLD namespace. We believe this gives is more freedom to optimize around unforseen constraints in the future. The use case is the same as Namecoin's "d/" namespace.</li>
+		        <li>Namecoin's name registration price is fixed at any given time and is independent of the name itself and implicit dividends are only paid when transaction fees are destroyed. Domainshares utilizes an auction-like mechanic to incentivize price discovery for names, making sure the final owner pays what it is actually worth. At least half of the final price will have gone to the network as dividends.</li>
+		    </ul>
+		    </div>
+		    
+		  </div>
+		</div>
+		
+		<div class="panel panel-default">
+		
+		  <div class="panel-heading">
+		    <h4 class="panel-title">
+		      <a data-toggle="collapse" data-parent="#accordion" href="#faq-ethereum">
+		        How does this project compare to Ethereum?
+		      </a>
+		    </h4>
+		  </div>
+		  
+		  <div id="faq-ethereum" class="panel-collapse collapse">
+		    <div class="panel-body">
+		    Ethereum is an runtime environment on a blockchain - you can easily make a TLD just like .p2p in just a few lines of code. A simple name registration script is shown on the ethereum website:
+		    
+		    <pre>if tx.value < block.basefee * 200:
+    stop
+if contract.storage[tx.data[0]] or tx.data[0] < 100:
+    stop
+contract.storage[tx.data[0]] = tx.data[1]
+			</pre>
+		    
+		    You can imagine completely mimicking .p2p's behavior in just a few hundred lines of code in this language. Allocating names on a blockchain is almost as basic as distributing shares and so we should expect to see namespace capabilities in practically every bitcoin 2.0 project.
+		    
+		    There are two tradeoffs to riding on general-purpose blockchains: one is that the blockchain developers cannot optimize the system around contraints relevant to this particular DAC, and the other is that the Ether unit of account does not reflect the success of any individual service and so there is no way for holders of the equity to profit from service performing its function well. The two systems also use different blockchain security models: bitshares DACs use DPOS while Ethereum uses a custom POW algorithm.
+		    </div>
+		    
+		  </div>
+		</div>
+		
+		<div class="panel panel-default">
+		
+		  <div class="panel-heading">
+		    <h4 class="panel-title">
+		      <a data-toggle="collapse" data-parent="#accordion" href="#faq-p2p-websites">
+		        How do I view .p2p websites?
+		      </a>
+		    </h4>
+		  </div>
+		  
+		  <div id="faq-p2p-websites" class="panel-collapse collapse">
+		    <div class="panel-body">
+		    You can view pages on the .p2p namespace without any installation or configuration by using a centralized proxy like dotp2p.io To use .p2p like a normal TLD, you need to configure your browser to point to a <a href="http://okturtles.com/">DNSchain</a> node, or install a browser extension like okTurtles.
+		    </div>
+		    
+		  </div>
+		</div>
+		
+		<div class="panel panel-default">
+		
+		  <div class="panel-heading">
+		    <h4 class="panel-title">
+		      <a data-toggle="collapse" data-parent="#accordion" href="#faq-tld">
+		        Why only one TLD?
+		      </a>
+		    </h4>
+		  </div>
+		  
+		  <div  id="faq-tld" class="panel-collapse collapse">
+		    <div class="panel-body">
+		   <p> 
+		       The original purpose of distinct TLD was to establish different rulesets for how the namespace is managed. The fact that it can be used to avoid name collisions is a secondary effect and an artifact of how traditional DNS is structured. AAPL.com and AAPL.org aren't just avoiding a collision - AAPL.org is utilizing the fact that it won't get sued on the .org namespace, while it would on the .com namespace. Sure you can make it so that two TLDs use the same ruleset and are on the same blockchain, but then the only thing that it accomplishes is to make an aesthetic difference between typing "yoursite-org / yoursite-com" and "yoursite.org / yoursite.com". One blockchain is one namespace, no matter how you slice it or mask it.
+		    
+</p>
+		    </div>
+		    
+		  </div>
+		</div>
+		
+		<div class="panel panel-default">
+		
+		  <div class="panel-heading">
+		    <h4 class="panel-title">
+		      <a data-toggle="collapse" data-parent="#accordion" href="#faq-expire">
+		         Do domain names expire?
+		      </a>
+		    </h4>
+		  </div>
+		  
+		  <div id="faq-expire" class="panel-collapse collapse">
+		    <div class="panel-body">
+		    To renew a domain, you just have to do any standard transaction, like sending it to yourself, at least once per year. After one year of inactivity, names are reclaimed by the network.
+		    </div>
+		    
+		  </div>
+		</div>
+	
+
+	</div> <!-- accordion -->
     </div>
 </div>
 <?php include '../footer.php'?>
